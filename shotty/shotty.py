19f345404d49826fba188a5cdc02f447f42b20ec
Jonathan <jh@userdefender.com>
2020-07-09 02:06:22 -0500

    Added list_all option for list_snapshots

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -24,7 +24,8 @@ def snapshots():
 
 @snapshots.command('list')
 @click.option('--project', default=None, help="Only snapshots for project (tag Project: <name>)")
-def list_snapshots(project):
+@click.option('--all', 'list_all', default=False, is_flag=True, help="List all snapshots for each volume regardless of status")
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
     instances = filter_instances(project)
@@ -40,6 +41,10 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+#Values returned via boto3 are most recent.  The following breaks the iteration
+#after the first (and most recent) completed snapshot is found,
+#assuming list_all isn't True.
+                if s.state == 'completed' and not list_all: break
 
     return
 
